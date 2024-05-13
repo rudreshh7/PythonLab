4ad7ad8dcf865c358e398afdb35757477febad12
Rudresh Jha <kumarrudresh5@gmail.com>
2024-05-13 12:48:58 +0530

    first commit

---
+++ StringOperation.py
@@ -0,0 +1,15 @@
+print("Hello Rudresh")
+
+str = " Hello King"
+
+
+print(str[0])
+print(str[-1])
+print(str[0:6])
+print(str[::1])
+print(str[::-2])
+
+
+str = "My name is Rudresh"
+str2 = "I am from XYZ"
+str3 = ("Hi" , "This","IS","King")
